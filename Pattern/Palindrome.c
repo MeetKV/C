f6ac87d9ab6c234c93df2bae94f47eaf439eab3a
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-22 18:12:45 +0530

    Add files via upload

---
+++ Pattern/Palindrome.c
@@ -0,0 +1,35 @@
+//    1
+//   121
+//  12321
+// 1234321
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+
+        for(int j=i-1;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
