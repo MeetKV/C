15cd077544b6fb7fc24d21041f91fc3e77d665c5
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-22 18:11:00 +0530

    Add files via upload

---
+++ Pattern/Floyd's triangle.c
@@ -0,0 +1,20 @@
+// 1
+// 23
+// 456
+// 78910
+
+#include<stdio.h>
+int main()
+{
+    int num=1;
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("  %d",num);
+            num++;            
+        }
+        printf("\n");
+    }
+    return 0;
+}
