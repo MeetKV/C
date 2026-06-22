39a8c5cc1614a799a41e8ed6a51941b91e4b9c84
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-22 18:12:54 +0530

    Add files via upload

---
+++ Pattern/Reverse same no.c
@@ -0,0 +1,20 @@
+// 11111
+// 2222
+// 333
+// 44
+// 5
+
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=5;j>=i;j--)
+        {
+            printf("%d",i);
+        
+        }
+        printf("\n");
+    }
+    return 0;
+}
