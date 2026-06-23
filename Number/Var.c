6107f3935012f60d8e66997f7901f20781949c18
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-23 10:14:05 +0530

    Add files via upload

---
+++ Number/Var.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    c = a * b;
+
+    printf("Multiplication = %d", c);
+
+    return 0;
+}
