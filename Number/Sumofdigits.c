acbc2ab880f2051078bd52b3a6b6be24cd3243a9
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-23 10:14:00 +0530

    Add files via upload

---
+++ Number/Sumofdigits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, sum = 0, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(temp != 0)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    printf("Sum of digits of %d is %d", n, sum);
+
+    return 0;
+}
