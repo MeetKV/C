f5c2f54d10d810990ec83259f77c8608d78202fa
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-23 10:13:51 +0530

    Add files via upload

---
+++ Number/OE.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a no:");
+    scanf("%d", &a);
+
+    if (a%2==0)
+    {
+        printf("No is Even");
+    }
+    else
+    {
+        printf("No is Odd");
+    }
+    return 0;    
+
+}
