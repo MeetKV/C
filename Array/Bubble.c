e48088ccfec47663575e32ada004770953633f01
Meet Kathrotiya <181106126+MeetKV@users.noreply.github.com>
2026-06-23 16:44:52 +0530

    Add files via upload

---
+++ Array/Bubble.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[10],temp;
+
+    printf("enter size of the arry:-");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++)
+    {
+        printf("a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+    return 0;
+}
